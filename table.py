260ed304c5710f763195ef4450806a25003f479b
Xiangyu Zhou <xiangz28@uw.edu>
2020-07-20 13:01:08 -0700

    revise check_function, add tests

--- table.py
+++ table.py
@@ -5,58 +5,59 @@ import json
 '''
 a table is represented by set of cells
 '''
-class Table:
+class Annotated_table:
 
     # construct the table with the given dataset.
-    def __init__(self, dataset, tuples):
-        self.df = tuples  # a set of tuples
-        self.load_and_clean_dataframe(dataset)
+    def __init__(self, cells):
+        self.df = cells  # a set of cells
+        #self.load_and_clean_dataframe(dataset)
 
-    '''
-    check if the set of values stored in target table is a subset of the set
-    of values stored in this table, if the argument contained in the trace of
-    tuple stored in target table is a subset of argument in the corresponding trace,
-    and check if operators match.
-    '''
-    def check_function(self, target):
-        for target_tuple in target.df:
-            exist = False
-            for this_tuple in self.df:
-                # check if there is at least one tuple in this table
-                # that is a parent of the target tuple
-                if target_tuple.is_subset_of(this_tuple):
-                    exist = True
-            if not exist:
-                return False
-        return True
-
-
-    def load_and_clean_dataframe(df):
-        pass
-
-    def get_df(self):
-        pass
 
 '''
-this class represents a tuple stored in the data frame with its trace
+check if the set of values stored in target table is a subset of the set
+of values stored in this table, if the argument contained in the trace of
+tuple stored in target table is a subset of argument in the corresponding trace,
+and check if operators match.
 '''
-class Tuple:
+def check_function(actual, target):
+    for target_cell in target.df:
+        exist = False
+        for this_cell in actual.df:
+            # check if there is at least one tuple in this table
+            # that is a parent of the target tuple
+            if target_cell.is_subset_of(this_cell):
+                exist = True
+        if not exist:
+            return False
+    return True
 
-    def __init__(self, values, argument, operator):
-        self.values = values
+
+def load_and_clean_dataframe(df):
+    pass
+
+def get_df(self):
+    pass
+
+'''
+this class represents a cell stored in the data frame with its trace
+'''
+class Table_cell:
+
+    def __init__(self, value, argument, operator):
+        self.value = value
         self.argument = argument
         self.operator = operator
 
-    def get_value(self, index):
-        return self.values[index]
+    def get_value(self):
+        return self.value
 
     def is_subset_of(self, other):
         # check this argument is a subset of target argument
-        if self.argument > other.argument:
+        if not self.argument.issubset(other.argument):
             return False
         if self.operator != other.operator:
             return False
-        if self.values != other.values:
+        if self.value != other.value:
             return False
         return True
 
--- table_test.py
+++ table_test.py
@@ -1,40 +1,45 @@
 from table import *
 import unittest
 
-class Table_test(unittest.TestCase):
 
-    def test_check_function(self):
+class TableTest(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TableTest, self).__init__(*args, **kwargs)
         tuples1 = set()
-        tuples1.add(Tuple([1, 2], set([1, 2]), 'sum'))
-        tuples1.add(Tuple([1, 2], set([1, 2]), 'sum'))
-        tuples1.add(Tuple([2, 2, 3], set([2, 3]), 'sum'))
-        tuples1.add(Tuple([1, 2, 3], set([3, 3]), 'sum'))
-        tuples1.add(Tuple([2, 2, 3], set([2, 3]), 'avg'))
-        a = Table(None, tuples1)
+        tuples1.add(Table_cell(3, set([1, 2]), 'sum'))
+        tuples1.add(Table_cell(1, set([1]), 'sum'))
+        tuples1.add(Table_cell(5, set([2, 3]), 'sum'))
+        tuples1.add(Table_cell(3, set([3, 3]), 'avg'))
+        tuples1.add(Table_cell(3, set([2, 4]), 'avg'))
+        self.a = Annotated_table(tuples1)
 
+    def test_check_function_general(self):
         tuples2 = set()
-        tuples2.add(Tuple([2, 2, 3], set([2, 3]), 'sum'))
+        tuples2.add(Table_cell(3, set([2]), 'sum'))
+        b = Annotated_table(tuples2)
+        self.assertTrue(check_function(self.a, b))
 
+    def test2(self):
         tuples3 = set()
-        tuples3.add(Tuple([2, 2, 3], set([2]), 'sum'))
-        tuples3.add(Tuple([1, 2], set([1]), 'sum'))
+        tuples3.add(Table_cell(5, set([2]), 'sum'))
+        tuples3.add(Table_cell(5, set([3]), 'sum'))
+        c = Annotated_table(tuples3)
+        self.assertTrue(check_function(self.a, c))
 
+    def test3(self):
         tuples4 = set()
-        tuples4.add(Tuple([2, 2, 3], set([2, 3, 4]), 'sum'))
-        tuples4.add(Tuple([1, 2], set([1]), 'sum'))
+        tuples4.add(Table_cell(1, set([]), 'sum'))
+        tuples4.add(Table_cell(5, set([]), 'sum'))
+        d = Annotated_table(tuples4)
+        self.assertTrue(check_function(self.a, d))
 
+    def test4(self):
         tuples5 = set()
-        tuples5.add(Tuple([2, 2, 3], set([2, 3]), 'avg'))
-
-        b = Table(None, tuples2)
-        c = Table(None, tuples3)
-        d = Table(None, tuples4)
-        e = Table(None, tuples5)
-
-        self.assertTrue(a.check_function(b))
-        self.assertTrue(a.check_function(c))
-        self.assertFalse(a.check_function(d))
-        self.assertFalse(a.check_function(e))
+        tuples5.add(Table_cell(3, set([1, 2, 4]), 'avg'))
+        e = Annotated_table(tuples5)
+        print(check_function(self.a,e))
+        self.assertFalse(check_function(self.a,e))
 
 
 if __name__ == '__main__':
